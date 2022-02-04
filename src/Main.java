d7594aee3e3e04e1b142a10d7d369eb98b070f7c
Инна Толмачева <rodichka.inna@mail.ru>
2022-02-04 10:23:05 +0300

    init commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //входные данные
+        int billStarted = 200;   // начальный счет
+        int summa = 700;         // сумма пополнения
+        int limit = 1000;        // сумма пополнения с которой начисляется бонус
+        int amount = 100;        // за каждые 100 рублей 1 бонусный рубль
+
+        // начисляем бонусные рубли
+        int bonus;
+        if (summa > limit) {
+            bonus = summa / amount;
+        } else {
+            bonus = 0;
+        }
+
+        // конечный остаток на счету
+        int bill = billStarted + bonus + summa;
+
+        System.out.println("Начистено " + bonus + " бонусных рублей");
+        System.out.println("Всего на счету " + bill + " рублей");
+
+
+    }
+
+}
